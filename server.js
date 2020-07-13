7c0ce8099155d2feffdfc7adf42e2e1f30e197bc
ZoeGeop <zoegeop@gmail.com>
2020-07-12 20:34:57 -0500

    make it better

---
+++ server.js
@@ -0,0 +1,9 @@
+const app = express();
+
+app.use(express.static("./dist/sitio-videos"));
+
+app.get("/*", (req, res) =>
+  res.sendFile("index.html", { root: "dist/sitio-videos/" })
+);
+
+app.listen(process.env.PORT || 8080);
